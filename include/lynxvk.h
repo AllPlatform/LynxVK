e058c6805cb7690f191e7662408127b4b42f0077
AkiraYuki <44525248+AllPlatform@users.noreply.github.com>
2025-09-16 12:12:09 +0700

    Create lynxvk.h

---
+++ include/lynxvk.h
@@ -0,0 +1,11 @@
+#ifndef LYNXVK_H
+#define LYNXVK_H
+
+// Include necessary headers from the Termux
+#include <android/native_window.h>
+#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_android.h>
+#include <vulkan/vulkan_xlib.h>
+#include <X11/Xlib.h>
+
+#endif // LYNXVK_H
